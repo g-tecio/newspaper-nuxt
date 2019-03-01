e083f4ae57c279e7dcec46a63390fe4496aeb44a
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2019-02-28 23:10:17 -0600

    categories routes TODO fix borders and components

--- components/Categories.vue
+++ components/Categories.vue
@@ -1,125 +1,136 @@
 <template>
-    <div class="navigation-section">
-        
-        <b-navbar class="navbar" toggleable="md" type="dark" variant="info">
-            <b-container class="navbar m-menu navbar-default">
-                <b-navbar-toggle target="nav_collapse">Topics</b-navbar-toggle>
+  <div class="navigation-section">
+    <b-navbar class="navbar" toggleable="md" type="dark" variant="info">
+      <b-container class="navbar m-menu navbar-default">
+        <b-navbar-toggle target="nav_collapse">Topics</b-navbar-toggle>
 
-                <b-collapse is-nav id="nav_collapse">
-                    <b-navbar-nav class="nav navbar-nav main-nav">
-                        <b-nav-item href="#">World</b-nav-item>
-                        <b-nav-item href="#">Politics</b-nav-item>
-                        <b-nav-item href="#">Business</b-nav-item>
-                        <b-nav-item href="#">Opinion</b-nav-item>
-                        <b-nav-item href="#">Tech</b-nav-item>
-                        <b-nav-item href="#">Science</b-nav-item>
-                        <b-nav-item href="#">Health</b-nav-item>
-                        <b-nav-item href="#">Sports</b-nav-item>
-                        <b-nav-item href="#">Arts</b-nav-item>
-                        <b-nav-item href="#">Books</b-nav-item>
-                        <b-nav-item href="#">Style</b-nav-item>
-                        <b-nav-item href="#">Food</b-nav-item>
-                        <!-- <b-nav-item href="#">Travel</b-nav-item>
-                        <b-nav-item href="#">Magazine</b-nav-item> -->
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-container>
-        </b-navbar>
-    </div>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav class="nav navbar-nav main-nav">
+            <b-nav-item v-for="category in categories" :key="category.id" :to="{name: 'category-id', params: { id: category.name, img: category.img } }">{{ category.name }}</b-nav-item>
+            <!-- <b-nav-item href="#">Travel</b-nav-item>
+            <b-nav-item href="#">Magazine</b-nav-item>-->
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        { name: "World", img: '38' },
+        { name: "Politics", img: '39' },
+        { name: "Business", img: '40'},
+        { name: "Opinion", img: '41' },
+        { name: "Tech", img: '42' },
+        { name: "Science", img: '43' },
+        { name: "Health", img: '45' },
+        { name: "Sports", img: '46' },
+        { name: "Arts", img: '47' },
+        { name: "Books", img: '48' },
+        { name: "Style", img: '49' },
+        { name: "Food", img: '50' }
+      ]
+    };
+  }
+};
+</script>
+
+
 <style>
-    .navigation-section i {
-        margin-left: 5px;
-        margin-top: 0 !important;
-    }
+.navigation-section i {
+  margin-left: 5px;
+  margin-top: 0 !important;
+}
 
-    .navigation-section .navbar-default {
-        border-top: 1px solid #000;
-        border-bottom: 2px solid #000;
-        margin-top: 50px;
-        background: transparent;
-        border-radius: 0;
-    }
+.navigation-section .navbar-default {
+  border-top: 1px solid #000;
+  border-bottom: 2px solid #000;
+  margin-top: 50px;
+  background: transparent;
+  border-radius: 0;
+}
 
-    @media screen and (max-width: 768px) {
-        .navigation-section .navbar-default {
-            border: none;
-            margin: 0;
-            display: inline;
-            padding: 0;
-        }
+@media screen and (max-width: 768px) {
+  .navigation-section .navbar-default {
+    border: none;
+    margin: 0;
+    display: inline;
+    padding: 0;
+  }
 
-        .nav {
-            border-bottom: #000 solid 1px;
-        }
-    }
+  .nav {
+    border-bottom: #000 solid 1px;
+  }
+}
 
-    @media (min-width: 768px) {
-        .navbar-default {
-            background-color: #fff;
-            padding: 5px 0;
-            border-color: transparent;
-            -webkit-transition: all .5s ease-in-out;
-            -moz-transition: all .5s ease-in-out;
-            transition: all .5s ease-in-out;
-        }
-        .navbar-right .dropdown-menu {
-            right: auto;
-        }
-    }
+@media (min-width: 768px) {
+  .navbar-default {
+    background-color: #fff;
+    padding: 5px 0;
+    border-color: transparent;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+  }
+  .navbar-right .dropdown-menu {
+    right: auto;
+  }
+}
 
-    .navbar-default {
-        margin-bottom: 0;
-    }
+.navbar-default {
+  margin-bottom: 0;
+}
 
-    .navbar-brand {
-        padding-top: 0;
-        padding-bottom: 0;
-        height: 40px;
-    }
+.navbar-brand {
+  padding-top: 0;
+  padding-bottom: 0;
+  height: 40px;
+}
 
-    .navbar-default .navbar-nav > li {
-        margin-right: 20px;
-    }
+.navbar-default .navbar-nav > li {
+  margin-right: 20px;
+}
 
-    .navbar-default .navbar-nav > li:last-child {
-        margin-right: 0px;
-    }
+.navbar-default .navbar-nav > li:last-child {
+  margin-right: 0px;
+}
 
-    .navbar-default .navbar-nav > li > a {
-        color: #000;
-        padding: 8px 17px;
-        text-transform: uppercase;
-        font-weight: bold;
-    }
+.navbar-default .navbar-nav > li > a {
+  color: #000;
+  padding: 8px 17px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
 
-    .navbar-toggler {
-        background: #FFF;
-        color: #A42242 !important;
-        border: #A42242 solid 1px !important;
-        border-radius: 5px;
-        width: 100%;
-        position: relative;
-    }
+.navbar-toggler {
+  background: #fff;
+  color: #a42242 !important;
+  border: #a42242 solid 1px !important;
+  border-radius: 5px;
+  width: 100%;
+  position: relative;
+}
 
-    .navbar-default .navbar-nav > .active > a,
-    .navbar-default .navbar-nav > .active > a:focus,
-    .navbar-default .navbar-nav > .active > a:hover {
-        background-color: transparent;
-    }
+.navbar-default .navbar-nav > .active > a,
+.navbar-default .navbar-nav > .active > a:focus,
+.navbar-default .navbar-nav > .active > a:hover {
+  background-color: transparent;
+}
 
-    .navbar-default .navbar-nav > li > a:focus,
-    .navbar-default .navbar-nav > li > a:hover {
-        color: #000;
-    }
+.navbar-default .navbar-nav > li > a:focus,
+.navbar-default .navbar-nav > li > a:hover {
+  color: #000;
+}
 
-    .nav-link {
-        color: #000 !important;
-    }
+.nav-link {
+  color: #000 !important;
+}
 
-    .navbar-expand-md .navbar-nav{
-        margin-left: 35px;
-    }
-
+.navbar-expand-md .navbar-nav {
+  margin-left: 35px;
+}
 </style>
---
+++ pages/category/_id.vue
@@ -0,0 +1,157 @@
+<template>
+  <div>
+    <Header/>
+    <Categories/>
+    <NewsBanner/>
+    <b-container>
+      <b-row>
+        <b-col md="4">
+          <h1 class="category-title">{{ $route.params.id }}</h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="category-container" md="4">
+          <b-container class="container-img category-main-art">
+            <b-img
+              class="article-img"
+              :src="'https://picsum.photos/1024/400/?image=' + $route.params.img"
+              fluid
+              alt="Responsive image"
+            />
+            <h2>This article is for the {{ $route.params.id }} category</h2>
+            <div class="article-date">
+              <b-row>
+                <b-col md="8">Thursday February, 7</b-col>
+                <b-col class="text-right" md="4">
+                  <b-link class="see-more text-right" href="#">See more...</b-link>
+                </b-col>
+              </b-row>
+            </div>
+          </b-container>
+
+          <b-container class="category-main-art">
+            <ArticleWithoutImg/>
+          </b-container>
+        </b-col>
+
+        <b-col class="border-bottom" md="8">
+          <b-row>
+            <b-col class="border-right" md="8">
+              <b-container class="container-img category-main-art">
+                <b-img
+                  class="article-img"
+                  :src="'https://picsum.photos/1024/400/?image=' + $route.params.img + 1"
+                  fluid
+                  alt="Responsive image"
+                />
+                <h2>This module is an intro to a full article of {{ $route.params.id }}</h2>
+                <div class="article-date">
+                  <b-row>
+                    <b-col md="8">Thursday February, 7</b-col>
+                    <b-col class="text-right" md="4">
+                      <b-link class="see-more text-right" href="#">See more...</b-link>
+                    </b-col>
+                  </b-row>
+                </div>
+              </b-container>
+            </b-col>
+            <b-col class="border-bottom" md="4">
+              <SideArticle/>
+              <SideArticle/>
+              <SideArticle/>
+            </b-col>
+
+            <b-col md="12">
+              <ComplexArticle/>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="4">
+          <h5 class="category-title"><strong>Latest</strong></h5>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="12">
+          <AfterFullContainer/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import Header from "~/components/Header.vue";
+import Categories from "~/components/Categories.vue";
+import NewsBanner from "~/components/NewsBanner.vue";
+import NewsContainer from "~/components/NewsContainer.vue";
+import ArticleWithoutImg from "~/components/articles/ArticleWithoutImg.vue";
+import ComplexArticle from "~/components/articles/ComplexArticle.vue";
+import SideArticle from "~/components/articles/SideArticle.vue";
+import SideNews from "~/components/SideNews.vue";
+import FullContainer from "~/components/FullContainer.vue";
+import AfterFullContainer from "~/components/AfterFullContainer.vue";
+import SideNews2 from "~/components/SideNews2.vue";
+export default {
+  components: {
+    Logo,
+    Header,
+    Categories,
+    NewsBanner,
+    NewsContainer,
+    SideNews,
+    FullContainer,
+    AfterFullContainer,
+    SideNews2,
+    ArticleWithoutImg,
+    SideArticle,
+    ComplexArticle
+  },
+  data() {
+    return {};
+  },
+  validate({ params }) {
+    // Must be a number
+    return params.id;
+  }
+};
+</script>
+
+<style>
+.border-right {
+  border-right: 1px #565656 solid;
+}
+.border-bottom {
+  border-bottom: 1px #565656 solid;
+}
+
+.category-container {
+  border-right: 1px #565656 solid;
+}
+.category-title {
+  border-block-end: 1px #565565 solid;
+  margin-top: 20px;
+}
+
+.category-main-art {
+  border-bottom: #565565 solid 1px;
+}
+
+.see-more {
+  color: #d2222a !important;
+}
+.article-date {
+  color: #a2a2a2;
+}
+.article-img {
+  width: 100%;
+  height: 260px;
+}
+.container-img {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
+
